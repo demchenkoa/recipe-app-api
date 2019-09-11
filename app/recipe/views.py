d0e05289d2195b8cfff9bc02e502d46360a681bb
alex_shavelev <alexshavelev44@gmail.com>
2019-09-11 16:21:13 +0300

    -> 63

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -51,3 +51,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """return appropriate serializer class"""
+
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+        return self.serializer_class
